e3c172b0b515d8141231bb98127ed77d1e5fa047
Angel Bas Gago <angelb1977@gmail.com>
2025-03-12 13:34:32 +0100

    Implementing Abstract class Animal

--- src/main/java/com/inserta/cat_api/entity/Animal.java
+++ src/main/java/com/inserta/cat_api/entity/Animal.java
@@ -1,5 +1,38 @@
 package com.inserta.cat_api.entity;
 
 public abstract class Animal {
+    private String especie;
+    private char alimento;
 
+    public Animal() {
+    }
+
+    public Animal(String especie, char alimento) {
+        this.especie = especie;
+        this.alimento = alimento;
+    }
+
+    public String getEspecie() {
+        return especie;
+    }
+
+    public void setEspecie(String especie) {
+        this.especie = especie;
+    }
+
+    public char getAlimento() {
+        return alimento;
+    }
+
+    public void setAlimento(char alimento) {
+        this.alimento = alimento;
+    }
+
+    @Override
+    public String toString() {
+        return "Animal{" +
+                "especie='" + especie + '\'' +
+                ", alimento=" + alimento +
+                '}';
+    }
 }
